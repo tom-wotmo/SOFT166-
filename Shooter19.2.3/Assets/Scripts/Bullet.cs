44e7bf1ac8fb21bde71983f82392cb1c4ee54aaa
tom-wotmo <geobite@hotmail.co.uk>
2019-11-25 16:50:51 +0000

    2C TASK 2

---
+++ Shooter19.2.3/Assets/Scripts/Bullet.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float moveSpeed = 100.0f;
+    public int damage = 1;
+    // Start is called before the first frame update
+    private void Start()
+    {
+        GetComponent<Rigidbody2D>().AddForce(transform.up * moveSpeed);
+        
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        other.transform.SendMessage("TakeDamage", damage, SendMessageOptions.DontRequireReceiver);
+    }
+    private void OnBecameVisible()
+    {
+        Die();
+        
+    }
+    private void Die()
+    {
+        Destroy(gameObject);
+    }
+
+ 
+}
