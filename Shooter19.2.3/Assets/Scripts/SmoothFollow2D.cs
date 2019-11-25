1c06fe53c2ee2861c95452869acfbd18d8f87fbc
tom-wotmo <geobite@hotmail.co.uk>
2019-11-25 15:31:15 +0000

    Week 5 completion
    All Week 5 implemented onto 3D platform on my mac

---
+++ Shooter19.2.3/Assets/Scripts/SmoothFollow2D.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class SmoothFollow2D : MonoBehaviour
+
+{
+    public Transform target;
+    public float smoothing = 5.0f;
+    // Start is called before the first frame updat
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+    
+      Vector3 newPos = new Vector3(target.position.x, target.position.y, transform.position.z);
+        transform.position = Vector3.Lerp(transform.position, newPos, (smoothing * 0.001f));   
+    }
+
+}
---
+++ Shooter19.2.3/Assets/Scripts/TopDownCharacterController2D.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class TopDownCharacterController2D : MonoBehaviour
+{
+    public float speed = 5.0f;
+
+    Rigidbody2D rigidbody2D;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+
+        rigidbody2D.velocity = new Vector2(x, y) * speed;
+        rigidbody2D.angularVelocity = 0.0f;
+    }
+}
