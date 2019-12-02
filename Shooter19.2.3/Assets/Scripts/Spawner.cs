a4eb52741d08a2bfbb2d6921ade48a9cff1cc949
tom-wotmo <geobite@hotmail.co.uk>
2019-12-02 21:41:17 +0000

    Listener added on Zombie, for explosion

---
+++ Shooter19.2.3/Assets/Scripts/Spawner.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+public class Spawner : MonoBehaviour
+{
+    public GameObject prefabToSpawn;
+    public float adjustmentAngle = 0;
+    public void Spawn()
+    {
+        Vector3 rotationInDegrees = transform.eulerAngles;
+        rotationInDegrees.z += adjustmentAngle;
+        Quaternion rotationInRadians = Quaternion.Euler(rotationInDegrees);
+        Instantiate(prefabToSpawn, transform.position, rotationInRadians);
+    }
+}
