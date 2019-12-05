9b5526802d563d83bd33a37f9b661a0ebb3f0db0
tom-wotmo <geobite@hotmail.co.uk>
2019-12-05 23:43:41 +0000

    4A

--- Shooter19.2.3/Assets/Scripts/Player.cs
+++ Shooter19.2.3/Assets/Scripts/Player.cs
@@ -1,7 +1,11 @@
 ﻿using UnityEngine;
 public class Player : MonoBehaviour
 {
+    public delegate void UpdateHealth(int newHealth);
+    public static event UpdateHealth OnUpdateHealth;
+
     private Animator gunAnim;
+
     private void Start()
     {
         gunAnim = GetComponent<Animator>();
@@ -15,6 +19,14 @@ public class Player : MonoBehaviour
         else
         {
             gunAnim.SetBool("isFiring", false);
+        }
+    }
+
+    public void SendHealthData(int health)
+    {
+        if (OnUpdateHealth != null)
+        {
+            OnUpdateHealth(health);
         }
     }
 }
