e73a451cde671acf4e3fcc07330ef74665fc3e21
tom-wotmo <geobite@hotmail.co.uk>
2019-12-10 13:34:17 +0000

    Added a new Player Death script
    a script that will now set the game object inactive when it dies instead od destroying it this allows me to active another game object when the character is set to inactive

---
+++ Shooter19.2.3/Assets/Scripts/GameOver.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class GameOver : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject endScreen;
+    // Update is called once per frame
+    void Update()
+    {
+        if(player.activeSelf == false)
+        {
+           endScreen.SetActive(true);
+        }
+        else
+        {
+            endScreen.SetActive(false);
+        }
+      
+      
+    }
+}
