ed88566149aa655c452ef15ebb8fcb0dac0941f2
tom-wotmo <geobite@hotmail.co.uk>
2019-12-11 15:24:46 +0000

    Added a menu for the UI

---
+++ Shooter19.2.3/Assets/Scripts/PauseUI.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseUI : MonoBehaviour
+{
+
+    public static bool GameIsPaused = false;
+    public GameObject _pauseMenu;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+        
+    }
+    void Resume()
+    {
+        _pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+
+    }
+    void Pause()
+    {
+        _pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+            
+    }
+}
