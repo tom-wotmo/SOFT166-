d2a144d8fd25800e8cad334cf09b55f4b62fc946
tom-wotmo <geobite@hotmail.co.uk>
2019-12-26 19:45:41 +0000

    First Game Build 0.0

---
+++ Shooter19.2.3/Assets/Scripts/PlayClip.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayClip : MonoBehaviour
+{
+
+    public AudioSource aSource;
+    public AudioClip aClip;
+
+    public void playClip()
+    {
+        aSource.clip = aClip;
+        aSource.Play();
+    }
+}
---
+++ Shooter19.2.3/Assets/Settings.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+
+public class Settings : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+
+    public void VolumeSilder (float volume)
+    {
+
+        //changing the exposed parameter value for our main mixer in exchange for the variable being passed in.
+        audioMixer.SetFloat("Volume", volume);
+    }
+    public void setFullscreen(bool isFullScreen)
+    {
+        Screen.fullScreen = isFullScreen;
+    }
+}
