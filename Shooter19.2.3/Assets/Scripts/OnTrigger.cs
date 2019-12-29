d32cfb5bb1468230f466f1855e53398aefae39a9
tom-wotmo <geobite@hotmail.co.uk>
2019-12-29 13:31:04 +0000

    Added spawn triggers

---
+++ Shooter19.2.3/Assets/Scripts/OnTrigger.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnTrigger : MonoBehaviour
+{
+    public Transform spawnLoc;
+    public GameObject spawnObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        // this will spawn the object when the player passes through it
+        Instantiate(spawnObject, spawnLoc.position, spawnLoc.rotation);
+
+        //this then destroys the game object so that you aren't continously spawning loads of them
+        Destroy(gameObject);
+
+    }
+
+
+}
---
+++ Shooter19.2.3/Assets/Scripts/TriggerEvent.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerEvent : MonoBehaviour
+{
+    public Transform spawn;
+    public GameObject spawnedobject;
+    // Start is called before the first frame update
+    void OnTriggerEnter()
+    {
+        Instantiate(spawnedobject, spawn.position, spawn.rotation);
+        
+    }
+
+   
+}
