c15c27f20772be975a0c0d7508310b203e3e9699
tom-wotmo <geobite@hotmail.co.uk>
2019-12-31 12:46:25 +0000

    Added a Pickup Manager
    Spawns in new pickups and regulates where they spawn

---
+++ Shooter19.2.3/Assets/Scripts/RandomSpawn.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomSpawn : MonoBehaviour
+{
+    public GameObject AttackSpeed, DamageIncrease, Invincible, MoveSpeed, Health;
+
+    public float spawntime = 5f;
+
+    float nextPickup = 0f;
+
+    int whatToSpawn;
+
+    void Update()
+    {
+        if (Time.time > nextPickup)
+        {
+            whatToSpawn = Random.Range(1, 6);
+            Debug.Log(whatToSpawn);
+        }
+        switch (whatToSpawn)
+        {
+            case 1:
+                Instantiate(AttackSpeed, transform.position, Quaternion.identity);
+                break;
+
+            case 2:
+                Instantiate(DamageIncrease, transform.position, Quaternion.identity);
+                break;
+
+            case 3: Instantiate(Invincible, transform.position, Quaternion.identity);
+                break;
+
+            case 4: Instantiate(MoveSpeed, transform.position, Quaternion.identity);
+                break;
+
+            case 5: Instantiate(Health, transform.position, Quaternion.identity);
+                break;
+
+        }
+        nextPickup = Time.time + spawntime;
+
+
+            
+    }
+}
---
+++ Shooter19.2.3/Assets/Scripts/RandomSpawnPickup.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomSpawnPickup
+    : MonoBehaviour
+     
+{
+	public GameObject pickupPrefab;
+	public float spawnTime = 5f;
+    public Transform spawnLoc;
+	// Start is called before the first frame update
+	void Start()
+	{
+        StartCoroutine(PickupRepeat());
+
+	}
+    private void SpawnPickup()
+	{
+        Instantiate(pickupPrefab, spawnLoc.position, spawnLoc.rotation);
+	}
+    IEnumerator PickupRepeat()
+	{
+		while (true)
+		{
+			yield return new WaitForSeconds(spawnTime);
+			SpawnPickup();
+		}
+	}
+
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+    
+	
+
+
+}
